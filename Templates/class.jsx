a8faddeebf98be06db100ff68da1871970e43929
Cemalettin Taş <cemalettin.tas.metu@gmail.com>
2020-07-06 16:18:04 +0300

    added class template

---
+++ Templates/class.jsx
@@ -0,0 +1,19 @@
+import React , {Component} from 'react';
+
+class Input extends  Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render(){
+
+    return (
+      <div className="Input">
+
+      </div>
+    )
+  }
+}
+
+export default Input
